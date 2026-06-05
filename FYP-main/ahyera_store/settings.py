9b6d6efe9b3919a6202c35092766c409752dd189
Ahmad Farukh <rajahmadfarukh@gmail.com>
2026-06-05 19:29:24 +0500

    Fix: Set ALLOWED_HOSTS to all for production

--- FYP-main/ahyera_store/settings.py
+++ FYP-main/ahyera_store/settings.py
@@ -18,9 +18,7 @@ SECRET_KEY = config(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="", cast=Csv())
-if not ALLOWED_HOSTS:
-    ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", default="", cast=Csv())
 
